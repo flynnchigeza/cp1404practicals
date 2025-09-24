e1f4f2bba4e1a3c206be0e9b0bd91c234cb10883
JD113245 <flynn.chigeza@my.jcu.edu.au>
2025-09-24 16:31:23 +1000

    Add score menu file

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -10,7 +10,6 @@ def main():
     while choice.upper() != "Q":
         print_menu()
         choice = input(">>> ").upper()
-
         if choice == "G":
             score = get_valid_score()
         elif choice == "P":
@@ -26,7 +25,7 @@ def main():
 
 def print_menu():
     """Display the menu options."""
-    print("\nMenu:")
+    print("Menu:")
     print("(G)et a valid score")
     print("(P)rint result")
     print("(S)how stars")
@@ -35,16 +34,12 @@ def print_menu():
 
 def get_valid_score():
     """Prompt user until a valid score (0–100 inclusive) is entered."""
-    score = -1
-    while score < 0 or score > 100:
-        try:
-            score = int(input("Enter a valid score (0-100): "))
-            if score < 0 or score > 100:
-                print("Invalid score, must be between 0 and 100.")
-        except ValueError:
-            print("Please enter a valid number.")
+    score = int(input("Enter a valid score (0-100): "))
+    if score < 0 or score > 100:
+        print("Invalid score, must be between 0 and 100.")
+    else :
+        print("Please enter a valid number.")
     return score
-
 
 def categorise_scores(score: float) -> str:
     """categorises users score"""
@@ -57,10 +52,8 @@ def categorise_scores(score: float) -> str:
     else:
         return "Bad"
 
-
 def show_stars(score):
     """Print as many stars as the score."""
     print("*" * score)
-
 
 main()
