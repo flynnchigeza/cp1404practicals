2b472b68b1401d659dd10a6668857c1fa3f41f6e
JD113245 <flynn.chigeza@my.jcu.edu.au>
2025-10-29 14:22:18 +1000

    Guitar_test file complete guitars file started

---
+++ prac_06/guitars.py
@@ -0,0 +1,20 @@
+"""
+CP1404 Guitars files from do from scratch
+"""
+from prac_06.guitar import Guitar
+
+
+def main():
+    """Guitar program, using Guitar class."""
+    guitars = []
+
+    print("My guitars!")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitar_to_add = Guitar(name, year, cost)
+        guitars.append(guitar_to_add)
+        print(guitar_to_add, "added.")
+        name = input("Name: ")
+
