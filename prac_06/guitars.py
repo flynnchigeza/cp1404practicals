ff482e355f01f28f754c6a783dfe2f2aebfc4af2
JD113245 <flynn.chigeza@my.jcu.edu.au>
2025-10-30 13:11:13 +1000

    Guitar_test file complete guitars file started prac 6 complete

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -18,3 +18,19 @@ def main():
         print(guitar_to_add, "added.")
         name = input("Name: ")
 
+        guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+        guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+    if guitars:
+        print("These are all of my guitars")
+        for i, guitar in enumerate(guitars, 1):
+            vintage_string = ""
+            if guitar.is_vintage():
+                vintage_string = "(vintage)"
+            print("Guitar {0}: {1.name:>20} ({1.year}), worth ${1.cost:10,.2f}{2}".format(i, guitar, vintage_string))
+        else:
+            print("No guitars :( Quick, go and buy one!")
+
+
+main()
+
